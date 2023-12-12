925299d34682f2eef3538701a8ba19e66e2d2dab
Gravizz <peeke0rr@gmail.com>
2023-12-12 15:15:23 +0700

    feat(ex-8):complete exercise

--- ex-6.js
+++ ex-6.js
@@ -2,13 +2,16 @@ const carCollection = ["toyota", "fiat", "honda", "bmw"];
 
 function updateCarCollection(carBrand) {
   // Start coding here
+  let carOrder = carCollection.indexOf(carBrand) + 1;
   if (!carCollection.includes(carBrand)) {
     carCollection.push(carBrand);
     return `new car collection is: ${carCollection}`;
   } else {
-    return `${carBrand} has already existed in the 1 position of car collection.`;
+    return `${carBrand} has already existed in the ${carOrder} position of car collection.`;
   }
 }
+
+console.log(carCollection.indexOf("honda"));
 
 //ผลลัพธ์ที่ควรได้จาก Example case
 //ในกรณีที่ยังไม่มียี่ห้อรถใน carCollection
