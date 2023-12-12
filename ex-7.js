e676e189b7bee6f9842d9943fec4ee9b3872e112
Gravizz <peeke0rr@gmail.com>
2023-12-12 14:34:54 +0700

    feat(ex-7):complete exercise

--- ex-7.js
+++ ex-7.js
@@ -1,8 +1,11 @@
 function isPalindrome(string) {
   // Start coding here
+  let reverseString = string.split("").reverse().join("");
+  console.log(reverseString);
+  return string == reverseString;
 }
 
 //Example case
-console.log(isPalindrome("reviver"));// true
-console.log(isPalindrome("บวบ"));// true
-console.log(isPalindrome("deliver"));// false
+console.log(isPalindrome("reviver")); // true
+console.log(isPalindrome("บวบ")); // true
+console.log(isPalindrome("deliver")); // false
